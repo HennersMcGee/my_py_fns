9cf5005840ac554514d6d667fa4c5877ab9a306a
HennersMcGee <henry_letton@hotmail.com>
2020-11-08 11:46:10 +0000

    Return __init__ to empty

--- my_py_fns/__init__.py
+++
@@ -1,9 +0,0 @@
-'''
-    File name: __init__.py
-    Author: Henry Letton
-    Date created: 2020-11-08
-    Python Version: 3.8.3
-    Description: Simple functions to test
-'''
-
-import my_py_fns.simple_functions
